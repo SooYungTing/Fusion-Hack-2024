01cd28ad307f6207370ca6f7f6e1a54d296cf934
Shayna003 <79242907+Shayna003@users.noreply.github.com>
2024-03-16 19:10:17 +0000

    added search by name function

--- flavorfusion/src/main/java/uk/ac/soton/flavorfusion/IngredientAPI.java
+++ flavorfusion/src/main/java/uk/ac/soton/flavorfusion/IngredientAPI.java
@@ -4,9 +4,52 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+class Meal {
+  String id;
+  String name;
+  String category;
+  String instructions;
+  ArrayList<Ingredient> ingredients;
+
+  public Meal(String id, String name, String category, String instructions)
+  {
+    this.id = id;
+    this.name = name;
+    this.category = category;
+    this.instructions = instructions;
+    ingredients = new ArrayList<>();
+  }
+
+  public void addIngredient(Ingredient ig)
+  {
+    ingredients.add(ig);
+  }
+
+  public String toString()
+  {
+    return "meal[id=" + id + ",name=" + name + ",category=" + category + ",instructions=" + instructions + ",ingredients=" + ingredients;
+  }
+}
+
+class Ingredient {
+  String name;
+  String measure;
+
+  public Ingredient(String name, String measure)
+  {
+    this.name = name;
+    this.measure = measure;
+  }
+
+  public String toString()
+  {
+    return "Ingredient[name=" + name + ",measure" + measure;
+  }
+}
 public class IngredientAPI {
   String searchByName= "https://www.themealdb.com/api/json/v1/1/search.php?s=";
   String filterByIngredient= "https://www.themealdb.com/api/json/v1/1/filter.php?i=";
@@ -14,8 +57,9 @@ public class IngredientAPI {
   StringBuffer response;
   JSONObject jsonResult;
 
-  public void searchByName(String name)
+  public ArrayList<Meal> searchByName(String name)
   {
+    ArrayList<Meal> mealsFound = new ArrayList<>();
     try {
       URL url = new URL(searchByName + name);
       HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -48,6 +92,8 @@ public class IngredientAPI {
           String strCategory = meal.getString("strCategory");
           String strInstructions = meal.getString("strInstructions");
 
+          Meal mealObj = new Meal(idMeal, strMeal, strCategory, strInstructions);
+
           System.out.println("Meal ID: " + idMeal);
           System.out.println("Meal Name: " + strMeal);
           System.out.println("Category: " + strCategory);
@@ -60,15 +106,19 @@ public class IngredientAPI {
             if (meal.has(ingredientKey) && !meal.isNull(ingredientKey) && !meal.getString(ingredientKey).isEmpty()) {
               String ingredient = meal.getString(ingredientKey);
               String measure = meal.getString(measureKey);
+              mealObj.addIngredient(new Ingredient(ingredient, measure));
               System.out.println(ingredient + ": " + measure);
             }
           }
+          mealsFound.add(mealObj);
           System.out.println("--------------------");
         }
       }
     } catch (Exception e) {
       e.printStackTrace();
     }
+    System.out.println(mealsFound);
+    return mealsFound;
   }
 
   public static void main(String[] args) {
