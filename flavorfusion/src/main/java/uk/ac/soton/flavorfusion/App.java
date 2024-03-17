0cdefef2d71c2bb2a3752228f8753abb10c103d8
Yung Ting <yts1g22@soton.ac.uk>
2024-03-17 11:33:17 +0000

    fixed the music media to run on windows and mac

--- flavorfusion/src/main/java/uk/ac/soton/flavorfusion/App.java
+++ flavorfusion/src/main/java/uk/ac/soton/flavorfusion/App.java
@@ -1,18 +1,49 @@
 package uk.ac.soton.flavorfusion;
 
+import java.net.URI;
 import java.net.URL;
 import uk.ac.soton.flavorfusion.ui.MainMenu;
 
 import javax.swing.*;
 import java.awt.*;
+import java.nio.file.Paths;
 import java.util.Objects;
 
 public class App {
     public static void main(String[] args) {
-        URL urlString = App.class.getResource("/music/track1.mp3");
+        /*URL urlString = App.class.getResource("/music/track1.mp3");
         System.out.println(urlString);
         Thread musicThread = new Thread(new MP3Player(urlString.getPath()));
         musicThread.start();
+        // Ensure the GUI is created on the Event Dispatch Thread for thread safety
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                // Create MainMenu and set it visible
+                MainMenu mainMenu = new MainMenu();
+                mainMenu.setVisible(true);
+
+                // Set custom icon for the main frame
+                setIconImage(mainMenu);
+            }
+        });
+    }*/
+        try {
+            URL url = App.class.getResource("/music/track1.mp3");
+            if (url != null) {
+                // Convert URL to URI to properly escape characters
+                URI uri = url.toURI();
+                // Convert URI to path, handling special characters and file system differences
+                String path = Paths.get(uri).toString();
+                System.out.println(path);
+                Thread musicThread = new Thread(new MP3Player(path));
+                musicThread.start();
+            } else {
+                System.out.println("Resource not found.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         // Ensure the GUI is created on the Event Dispatch Thread for thread safety
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
