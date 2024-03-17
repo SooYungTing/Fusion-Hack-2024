51bd1db9011f86aefa147810044b4ff07e8145bf
weartem13 <1758229153@qq.com>
2024-03-17 07:42:16 +0000

    Updated WeatherData.java with units and constructor

--- flavorfusion/src/main/java/uk/ac/soton/flavorfusion/WeatherData.java
+++ flavorfusion/src/main/java/uk/ac/soton/flavorfusion/WeatherData.java
@@ -1,10 +1,41 @@
 package uk.ac.soton.flavorfusion;
 
+import java.beans.Visibility;
+
 public class WeatherData
 {
   float temperature;
   int humidity;
+  float pressure;
+  //紫外线等级 国际上从0开始 到11+后无上限了
+  int uvIndex;
   String weatherCondition;
+
+  //风速: 单位km/h
+  float windSpeed;
+  //降水: 单位mm
+  float precipitation;
+  //能见度: 单位km
+  float visibility;
+  //白天或者黑夜:白天为1 黑夜为0 (api是这么给的)
+  int daynight;
+  //白云占天空占百分比 int %
+  int cloud;
+
+  public WeatherData(float temperature, int humidity, float pressure, int uvIndex,
+      String weatherCondition, float windSpeed, float precipitation, float visibility, int daynight,
+      int cloud) {
+    this.temperature = temperature;
+    this.humidity = humidity;
+    this.pressure = pressure;
+    this.uvIndex = uvIndex;
+    this.weatherCondition = weatherCondition;
+    this.windSpeed = windSpeed;
+    this.precipitation = precipitation;
+    this.visibility = visibility;
+    this.daynight = daynight;
+    this.cloud = cloud;
+  }
 
   public WeatherData(float temperature, int humidity, String weatherCondition)
   {
